25e427894f28e1e2be7e1f1ac4dbd923402aa12f
kazhar136 <101134984+kazhar136@users.noreply.github.com>
2022-09-18 14:08:26 -0700

    Delete Q-7.py

--- Q-7.py
+++
@@ -1,9 +0,0 @@
-'''Define a function reverse() that computes the reversal of a string. For example, 
-reverse(“I am testing”) should return the string “gnitset ma I”.'''
-
-def reverse(rev):
-    d=rev[::-1]
-    return d
-
-ob=reverse("I am testing")
-print(ob)
